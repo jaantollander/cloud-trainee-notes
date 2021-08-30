7b314e2fa55166aaf3f4ad0cd95ec96031180407
Jaan Tollander de Balsch <jaan@hey.com>
2021-08-30 13:51:43 +0300

    Formatting

--- presentation/03.03.application.jl
+++ presentation/03.03.application.jl
@@ -1,7 +1,9 @@
 cd("../GenieWebApp.jl")
 
+
 # Install dependencies for the web application
 using Pkg; Pkg.instantiate(); Pkg.activate(".")
+
 
 # Load the web application
 using Genie; Genie.loadapp(); up(8000)
